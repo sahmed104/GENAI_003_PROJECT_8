91d11156b546eedf1c088f7139ff1139006a1d98
Shehzad Ahmed <sahmed91@student.gsu.edu>
2025-04-28 03:34:58 -0400

    run.py fixed for Render

--- run.py
+++ run.py
@@ -2,12 +2,7 @@ from app import create_app
 from app.models import init_db
 
 app = create_app()
-init_db() 
-
-# DEBUG: print all routes
-print("Registered routes:")
-print(app.url_map)
 
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
-
